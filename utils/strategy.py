08be0787cba7ae7cacea258dfc8a2935fdd74178
Sergio Herrero <sergioherre96@gmail.com>
2022-02-01 19:18:27 +0100

    Mejoras visuales

--- utils/strategy.py
+++ utils/strategy.py
@@ -88,8 +88,8 @@ def calculate_probability(client, pair):
     data = binance.get_minute_data(client, pair, '1m', '100')
     rsi, macd, stoch_k, stoch_d, price = analysis.return_strategy_data(data)
     probability = rsi_probability(rsi) + stoch_probability(stoch_k, stoch_d) + macd_probability(data, macd)
-    print('El activo {0} se encuentra en los siguientes niveles: RSI: {1}, MACD: {2}, Estocástico: {3}/{4}, probabilidad {5}'.format(
-        pair, rsi, macd, stoch_k, stoch_d, probability))
+    print('El activo {0} se encuentra en los siguientes niveles: RSI: {1}, MACD: {2}, Estocástico: {3}/{4}, probabilidad: {5}%'.format(
+        pair, round(rsi, 4), round(macd, 4), round(stoch_k, 4), round(stoch_d, 4), round(probability, 2)))
     
     return probability, data
 
